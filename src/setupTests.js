44799d71dda377141948c868a8c6cf0fed0f1422
slavapavlutin <sl.pavlutin@gmail.com>
2018-06-19 17:57:19 +0300

    Update react-testing-library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
 /* eslint import/no-extraneous-dependencies: [0] */
+import 'react-testing-library/cleanup-after-each';
 import * as emotion from 'emotion';
 import { createSerializer } from 'jest-emotion';
 
