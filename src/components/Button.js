27ce87b183df4362b4e81ca749d24fb79fcc187b
slavapavlutin <sl.pavlutin@gmail.com>
2018-06-19 17:52:00 +0300

    Change Button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,13 @@ import _ from 'lodash';
 import styled from 'react-emotion';
 
 const Button = styled('button')`
-  padding: ${15 / 1.5}px 15px;
+  display: inline-block;
+  padding: 15px;
   border: 0;
   border-radius: 2px;
-  text-transform: uppercase;
-  font-size: 14px;
-  font-weight: bold;
   background: ${(p) => _.get(p, 'theme.colors.button')};
+  font-size: 15px;
+  font-weight: 500;
   color: ${(p) => _.get(p, 'theme.colors.buttonText')};
   cursor: pointer;
 
